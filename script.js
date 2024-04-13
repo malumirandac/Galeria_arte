adca3ce1a8bbe22b9975dabd559e590e21e2ccf6
Malu <f.malumiranda@gmail.com>
2024-04-12 20:49:49 -0400

    primer commit de la web

---
+++ script.js
@@ -0,0 +1,34 @@
+// Carrusel de Imágenes
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+const slideInterval = setInterval(nextSlide, 5000);
+
+function nextSlide() {
+    slides[currentSlide].classList.remove('active');
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].classList.add('active');
+}
+
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+
+prevBtn.addEventListener('click', () => {
+    clearInterval(slideInterval);
+    prevSlide();
+    slideInterval = setInterval(nextSlide, 5000);
+});
+
+nextBtn.addEventListener('click', () => {
+    clearInterval(slideInterval);
+    nextSlide();
+    slideInterval = setInterval(nextSlide, 5000);
+});
+
+function prevSlide() {
+    slides[currentSlide].classList.remove('active');
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    slides[currentSlide].classList.add('active');
+}
+
+// Otras funciones y eventos
+// ...
